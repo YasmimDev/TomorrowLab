a03186e7419c29bd2da6f31cfa97590d67ee476c
Louiexz <fluizlucas@gmail.com>
2023-10-09 14:57:47 -0300

    RIS-2 Atualizando arquivos

--- adicionarFundador.js
+++ adicionarFundador.js
@@ -1,7 +1,7 @@
 function adicionarFundador() {
-    const classe = document.getElementById('c-cad-startup__id-abaixo');
+    const classe = document.querySelector('.c-cad-startup__c-abaixo');
     const novoFundador = document.createElement('textarea');
     novoFundador.rows = 5;
     novoFundador.placeholder = 'Nome do fundador:\nSobrenome do fundador:\nResponsabilidades:\nExperiência profissional anterior:'
     classe.appendChild(novoFundador);
- }
+}
